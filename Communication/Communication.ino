89230877f60c1b818fdd45f84c6db61d4e1f6bbe
bperez6090 <35755781+bperez6090@users.noreply.github.com>
2019-01-20 17:09:44 -0800

    Add files via upload

---
+++ Communication/Communication.ino
@@ -0,0 +1,36 @@
+#include <Arduino.h>
+#include <U8g2lib.h>
+
+#ifdef U8X8_HAVE_HW_SPI
+#include <SPI.h>
+#endif
+#ifdef U8X8_HAVE_HW_I2C
+#include <Wire.h>
+#endif
+
+U8G2_SSD1306_128X32_UNIVISION_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);   // Adafruit Feather ESP8266/32u4 Boards + FeatherWing OLED
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  u8g2.begin();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  char input;
+while (true){
+  if (Serial.available() > 0) {             
+      input = Serial.read();
+
+
+      u8g2.clearBuffer();         
+      u8g2.setFont(u8g2_font_ncenB08_tr);
+      u8g2.drawStr(0, 10, "BMP - You did it"); 
+      u8g2.sendBuffer();          
+      delay(1000);
+    }
+}
+
